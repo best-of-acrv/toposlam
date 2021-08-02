94a900976afb8f2a3f0162f0389d82ddd201c69b
Ben Talbot <b.talbot@qut.edu.au>
2021-08-02 16:20:21 +1000

    Add __init__ with link to dummy class

---
+++ toposlam/toposlam.py
@@ -0,0 +1,16 @@
+class TopoSlam(object):
+
+    def __init__(self):
+        print("Created")
+
+    def run_batch(self):
+        print("Run batch")
+
+    def run_step(self):
+        print("Run step")
+
+    def update_data(self):
+        print("Updated data")
+
+    def update_global_pose(self):
+        print("Updated pose")
